20275abce29f4f5015e56180835a12e9ae6ea8bd
Milann <58353018+ItsMilann@users.noreply.github.com>
2020-08-01 08:49:43 +0545

    Delete index.js

--- frontend/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import { leads } from "../components/Leads";
-// import {errors} from '../components/Alerts'
-
-export default combineReducers({
-	leads,
-	//errors
-});
